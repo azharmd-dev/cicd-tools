58828785655f23887616535cc7c49bac20d23a02
azharmd-dev <azharmd0794@gmail.com>
2026-02-01 16:07:26 +0530

    ci cd tools

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "robomart"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "azharprojects.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z0553382WPE1E3VPCYS6"
+  description = "description"
+}
+
+# variable "sonar" {
+#   default = false
+# }
