779d6fd0260219b7366c05b20c0217e4bc7d1197
azharmd-dev <azharmd0794@gmail.com>
2026-02-05 10:14:42 +0530

    cicd tools

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
 set -xe
 
-# Install required packages (NO full system update)
-
 dnf install -y java-17-openjdk wget unzip cloud-utils-growpart lvm2
-
-# ===== Disk Resize =====
 
 growpart /dev/nvme0n1 4
 pvresize /dev/nvme0n1p4
 
-# Give all extra space to /var (best for SonarQube)
+# Expand root first (important for /opt)
+
+lvextend -L +10G /dev/mapper/RootVG-rootVol
+xfs_growfs /
+
+# Give rest to /var
 
 lvextend -l +100%FREE /dev/mapper/RootVG-varVol
 xfs_growfs /var
-
-# ===== Add Swap (important for small EC2 instances) =====
 
 fallocate -l 2G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 
-# ===== Install SonarQube =====
-
 cd /opt
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.6.0.92116.zip
+wget [https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.6.0.92116.zip](https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.6.0.92116.zip)
 unzip sonarqube-*.zip
 mv sonarqube-* sonarqube
 
-
 useradd sonar
 chown -R sonar:sonar /opt/sonarqube
-
-# Start SonarQube
 
 sudo -u sonar /opt/sonarqube/bin/linux-x86-64/sonar.sh start
