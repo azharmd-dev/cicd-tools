ed95c256b2b754adac5a5f37865832fbb46d21ed
azharmd-dev <azharmd0794@gmail.com>
2026-02-05 10:04:47 +0530

    cicd tools

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 set -xe
 
-# Install required packages ONLY (no full system update)
+# Install required packages (NO full system update)
 
-dnf install -y java-17-openjdk wget unzip cloud-utils-growpart
+dnf install -y java-17-openjdk wget unzip cloud-utils-growpart lvm2
 
-# Resize disk
+# ===== Disk Resize =====
 
 growpart /dev/nvme0n1 4
 pvresize /dev/nvme0n1p4
-lvextend -L +10G /dev/mapper/RootVG-varVol
-lvextend -L +5G /dev/mapper/RootVG-rootVol
-lvextend -l +100%FREE /dev/mapper/RootVG-homeVol
-xfs_growfs /
-xfs_growfs /var
-xfs_growfs /home
 
-# Add swap
+# Give all extra space to /var (best for SonarQube)
+
+lvextend -l +100%FREE /dev/mapper/RootVG-varVol
+xfs_growfs /var
+
+# ===== Add Swap (important for small EC2 instances) =====
 
 fallocate -l 2G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 
-# Install SonarQube
+# ===== Install SonarQube =====
 
 cd /opt
-wget [https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.6.0.92116.zip](https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.6.0.92116.zip)
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.6.0.92116.zip
 unzip sonarqube-*.zip
 mv sonarqube-* sonarqube
 
+
 useradd sonar
 chown -R sonar:sonar /opt/sonarqube
+
+# Start SonarQube
 
 sudo -u sonar /opt/sonarqube/bin/linux-x86-64/sonar.sh start
